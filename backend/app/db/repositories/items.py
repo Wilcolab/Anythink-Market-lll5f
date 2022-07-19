84703da2583a26fed2c10392898c588901441024
Jan Kral <jasiek.kral@gmail.com>
2022-07-19 10:14:48 +0200

    Fix#3 frontend solution

--- backend/app/db/repositories/items.py
+++ backend/app/db/repositories/items.py
@@ -21,7 +21,6 @@ from app.models.domain.users import User
 
 SELLER_USERNAME_ALIAS = "seller_username"
 SLUG_ALIAS = "slug"
-DEFAULT_IMAGE = "/placeholder.png"
 
 CAMEL_OR_SNAKE_CASE_TO_WORDS = r"^[a-z\d_\-]+|[A-Z\d_\-][^A-Z\d_\-]*"
 
@@ -277,7 +276,7 @@ class ItemsRepository(BaseRepository):  # noqa: WPS214
             title=title,
             description=item_row["description"],
             body=item_row["body"],
-            image=item_row["image"] if item_row["image"] else DEFAULT_IMAGE,
+            image=item_row["image"],
             seller=await self._profiles_repo.get_profile_by_username(
                 username=seller_username,
                 requested_user=requested_user,
--- frontend/src/components/ItemPreview.js
+++ frontend/src/components/ItemPreview.js
@@ -19,6 +19,9 @@ const mapDispatchToProps = (dispatch) => ({
 
 const ItemPreview = (props) => {
   const item = props.item;
+  if (item.image == null) {
+    item.image = "/placeholder.png"
+  }
 
   const handleClick = (ev) => {
     ev.preventDefault();
